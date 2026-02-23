2912a611eb696c62badc2d2fa184057ae962476b
Roman <m3f3dr0nbristleback@gmail.com>
2026-02-23 20:51:11 +0300

    feat(compound): create http connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Trend Feed!")
+	})
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error")
+	}
 }
